9f41aceb1eb0a87525827a4172a804145a25d6dd
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-11-27 09:49:36 +0300

    Add files via upload

---
+++ button.ornek.44/button.ornek.44/button.ornek.44/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button.ornek._44
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hasta hasta = new Hasta();
+            hasta.isim = "Büşra Merve Ekmen";
+            hasta.yas = 15;
+            hasta.tcNumara = 11111111111;
+            hasta.telefon=5555555555;
+            hasta.yakini = "Arkadaş";
+            hasta.bakiciAdi = "Meryem Aynur";
+
+            label1.Text = hasta.isim + "    " + hasta.yas + "    " + hasta.tcNumara + "    " + hasta.telefon + "    " + hasta.yakini + "    " + hasta.bakiciAdi + "    ";
+             
+        }
+    }
+}
---
+++ button.ornek.44/button.ornek.44/button.ornek.44/Hasta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace button.ornek._44
+{
+    internal class Hasta
+    {
+        public string isim;
+        public double yas;
+        public double tcNumara;
+        public double telefon;
+        public string yakini;
+        public string bakiciAdi;
+    }
+}
